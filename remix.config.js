7c5b0f15ffedc90ad771b6961d4428e6c25f2300
EdenDadou31 <ewisniewski@atecna.fr>
2025-01-03 02:19:05 +0100

    fix/animation-svg-4

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 const { withEsbuildOverride } = require("remix-esbuild-override");
 const svgrPlugin = require("@remix-run/esbuild-plugin-svgr");
 
-withEsbuildOverride((option, { isServer }) => {
+withEsbuildOverride((option) => {
   option.plugins = [
     svgrPlugin(),
     ...option.plugins, // ajoute les plugins existants
