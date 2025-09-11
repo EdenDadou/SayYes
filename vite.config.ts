a941ed0d9c2ddb269dd207cb23de1b91e00277c8
EdenDadou <edenw@hotmail.fr>
2025-09-12 01:04:02 +0200

    fix/beug deployement

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_routeConfig: true,
       },
       routes(defineRoutes) {
         return flatRoutes("routes", defineRoutes, {
