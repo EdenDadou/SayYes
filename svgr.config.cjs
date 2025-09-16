764c66d76bf1862adb557bc9e73fa3dd634b9e40
EdenDadou <edenw@hotmail.fr>
2025-09-16 22:35:03 +0200

    feat/solution mobile

--- svgr.config.cjs
+++ svgr.config.cjs
@@ -4,23 +4,9 @@ module.exports = {
   svgoConfig: {
     plugins: [
       {
-        name: "preset-default",
-        params: {
-          overrides: {
-            removeViewBox: false, // Garde le viewBox pour la responsivité
-            removeUselessStrokeAndFill: false, // Garde les styles pour la personnalisation
-          },
-        },
+        name: "removeViewBox",
+        active: false,
       },
-      "removeDimensions", // Supprime width/height pour rendre responsive
-      "sortAttrs", // Optimise l'ordre des attributs
     ],
-  },
-  // Optimisations supplémentaires
-  replaceAttrValues: {
-    "#000": "currentColor", // Permet la personnalisation des couleurs
-  },
-  svgProps: {
-    "aria-hidden": "true", // Améliore l'accessibilité
   },
 };
