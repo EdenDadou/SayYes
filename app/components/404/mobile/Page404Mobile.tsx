f0b14f2789a1b5d662cd0c97f4848d55fc044313
EdenDadou <edenw@hotmail.fr>
2025-10-29 11:08:49 +0100

    fix/404 mobile

--- app/components/404/mobile/Page404Mobile.tsx
+++ app/components/404/mobile/Page404Mobile.tsx
@@ -1,47 +1,51 @@
-import { Link } from "@remix-run/react";
-import { useState } from "react";
-import HeaderMobile from "~/components/Header/components/HeaderMobile";
-import ModalContact from "~/components/ModalContact";
+import { useNavigate } from "@remix-run/react";
+import MobileLayout from "~/components/Layout/Mobile";
+import SvgBtnLinkedin from "~/components/Footer/components/BtnLinkedin";
+import SvgBtnFacebook from "~/components/Footer/components/BtnFacebook";
+import SvgBtnInstagram from "~/components/Footer/components/BtnInstagram";
+import SvgBtnTiktok from "~/components/Footer/components/BtnTiktok";
+import SvgBtnYoutube from "~/components/Footer/components/BtnYoutube";
+import Button from "~/components/Button";
+import ArrowFull from "~/assets/icons/ArrowFull";
+import Background404 from "../Background404";
+import MasqueGif from "../MasqueGif";
+import Background404Mobile from "./Background404Mobile";
 
 export default function Page404Mobile() {
-  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
   return (
-    <div className="flex items-center justify-center w-screen h-full">
-      <ModalContact isOpen={isOpen} close={() => setIsOpen(false)} />
-      <div className="flex flex-col items-center justify-start w-screen bg-gray-600">
-        <HeaderMobile
-          setIsOpen={setIsOpen}
-          isIntroFinish={true}
-          isOpen={isOpen}
-        />
+    <MobileLayout footer={false}>
+      <main className="w-screen h-screen flex flex-col items-start justify-center px-8">
+        <Background404Mobile />
+
+        <div className="h-[3px] w-28 holographic-bg my-8" />
+
         <div
-          className="w-screen h-screen flex flex-col items-center justify-center"
+          className="w-[85vw] aspect-[16/10] z-10"
           style={{
-            backgroundImage: 'url("images/404/404.gif")',
-            backgroundSize: "120vh",
-            backgroundPositionY: "-40px",
-            backgroundPositionX: "-550px",
+            backgroundImage: 'url("images/404/Title404.png")',
             backgroundRepeat: "no-repeat",
+            backgroundSize: "80vw auto",
           }}
-        >
-          <div className="w-full relative">
-            <img
-              loading="lazy"
-              src="images/404/bgMobile.png"
-              alt="404"
-              className="w-full h-screen"
-            />
-            <Link
-              to="/"
-              replace
-              className="w-48 absolute bottom-[5%] right-[3%]"
-            >
-              <img loading="lazy" src="images/404/ctaSend.png" alt="404" />
-            </Link>
+        />
+        <div className="flex flex-col items-start justify-between h-40">
+          <Button
+            type="border"
+            onClick={() => navigate(`/`)}
+            leftIcon={<ArrowFull className="w-6 h-6" />}
+            label="Accueil"
+            textSize="L"
+          />
+          <div className="flex flex-row gap-3 z-20">
+            <SvgBtnLinkedin className="w-8 h-8" />
+            <SvgBtnFacebook className="w-8 h-8" />
+            <SvgBtnInstagram className="w-8 h-8" />
+            <SvgBtnTiktok className="w-8 h-8" />
+            <SvgBtnYoutube className="w-8 h-8" />
           </div>
         </div>
-        {/* <FooterMobile /> */}
-      </div>
-    </div>
+      </main>
+    </MobileLayout>
   );
 }
