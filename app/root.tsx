217d90862e2054f61c6dd60684ec1b6c7cdec853
EdenDadou <edenw@hotmail.fr>
2025-12-02 17:37:26 +0100

    fix/table landing page

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
-        <link rel="preload" as="image" href="/images/404/404.gif" />
         <Links />
       </head>
       <body>
