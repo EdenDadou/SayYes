38b3a6221b1c7897aeb5c523fce5c2c6714823ab
EdenDadou <edenw@hotmail.fr>
2025-12-11 19:44:32 +0100

    fix/test

--- app/components/Screens/PortfolioProject/BackgroundProject1.tsx
+++ app/components/Screens/PortfolioProject/BackgroundProject1.tsx
@@ -7,6 +7,7 @@ const gpuOptimizedStyle: CSSProperties = {
   backfaceVisibility: "hidden",
   WebkitBackfaceVisibility: "hidden",
   contain: "layout paint",
+  pointerEvents: "none",
 };
 
 interface BackgroundProject1Props extends SVGProps<SVGSVGElement> {
@@ -16,7 +17,7 @@ interface BackgroundProject1Props extends SVGProps<SVGSVGElement> {
 const BackgroundProject1 = memo(function BackgroundProject1(
   props: BackgroundProject1Props
 ) {
-  const { isMobile, ...svgProps } = props;
+  const { isMobile: _deprecatedIsMobileProp, ...svgProps } = props;
 
   // Fonction pour calculer une couleur plus sombre basée sur la couleur principale
   const getDarkerColor = (baseColor: string) => {
@@ -38,9 +39,18 @@ const BackgroundProject1 = memo(function BackgroundProject1(
   const baseColor = svgProps.fill || "#1255CB";
   const darkerColor = getDarkerColor(baseColor);
 
-  // Réduire les valeurs de blur sur mobile pour de meilleures performances
-  // tout en gardant le même rendu visuel
-  const blurScale = isMobile ? 0.4 : 1;
+  // Styles de blur CSS (utilisés pour desktop et mobile)
+  const getBlurStyle = (blurValue: number): CSSProperties => ({
+    filter: `blur(${blurValue}px)`,
+    WebkitFilter: `blur(${blurValue}px)`,
+    willChange: "filter",
+    transform: "translateZ(0)",
+    WebkitTransform: "translateZ(0)",
+    isolation: "isolate" as const,
+  });
+
+  const blurScale = 1;
+  const blur = (value: number) => getBlurStyle(value * blurScale);
 
   return (
     <svg
@@ -67,7 +77,7 @@ const BackgroundProject1 = memo(function BackgroundProject1(
           >
             <path fill="#080809" d="M.188 0h1280v704H.188z" />
           </mask>
-          <g filter="url(#bg1-d)" mask="url(#bg1-c)">
+          <g mask="url(#bg1-c)" style={blur(274.777)}>
             <ellipse
               cx={152.646}
               cy={781.921}
@@ -78,26 +88,26 @@ const BackgroundProject1 = memo(function BackgroundProject1(
             />
           </g>
         </g>
-        <g filter="url(#bg1-e)">
+        <g style={blur(43.65)}>
           <path
             fill={darkerColor}
             d="M-226.515 218.584c25.703-198.081 165.722 225.868 811.554 387.791 530.061 151.46 921.211-144.56 895.511 53.521S1101.82 1376.83 513.081 1227.6-252.218 416.666-226.515 218.584"
           />
         </g>
-        <g filter="url(#bg1-f)">
+        <g style={blur(68.65)}>
           <path
             fill={svgProps.fill || "#1255CB"}
             d="M-227.445 310.023c27.367-210.9 164.227 237.387 810.074 399.198 529.921 152.532 924.281-168.247 896.921 42.654-27.37 210.9-385.34 767.865-974.078 618.635-588.74-149.23-760.283-849.587-732.917-1060.487"
           />
         </g>
-        <g filter="url(#bg1-g)">
+        <g style={blur(43.65)}>
           <path
             fill="#fff"
             d="M-382.81 885.501c12.557-270.696 393.198-114.418 905.36-71.661 512.16 42.756 961.9-44.201 949.34 226.49-12.55 270.7-437.92 455.48-950.085 412.73C9.642 1410.3-395.368 1156.2-382.81 885.501"
           />
         </g>
       </g>
-      <g filter="url(#bg1-h)">
+      <g style={blur(54.917)}>
         <ellipse
           cx={732.708}
           cy={190.536}
@@ -107,7 +117,7 @@ const BackgroundProject1 = memo(function BackgroundProject1(
           transform="matrix(-.81582 .5783 .60034 .79975 1540.98 -633.949)"
         />
       </g>
-      <g filter="url(#bg1-j)">
+      <g style={blur(19.221)}>
         <ellipse
           cx={706.052}
           cy={41.396}
@@ -117,7 +127,7 @@ const BackgroundProject1 = memo(function BackgroundProject1(
           transform="matrix(-.81582 .5783 .60034 .79975 1835.73 -660.531)"
         />
       </g>
-      <g filter="url(#bg1-l)">
+      <g style={blur(15)}>
         <ellipse
           cx={729.285}
           cy={191.517}
@@ -127,7 +137,7 @@ const BackgroundProject1 = memo(function BackgroundProject1(
           transform="matrix(-.72914 .68437 .71336 .7008 1585.5 -693.998)"
         />
       </g>
-      <g filter="url(#bg1-n)">
+      <g style={blur(19.221)}>
         <ellipse
           cx={702.752}
           cy={24.468}
@@ -138,110 +148,6 @@ const BackgroundProject1 = memo(function BackgroundProject1(
         />
       </g>
       <defs>
-        <filter
-          id="bg1-d"
-          width={2339.05}
-          height={2100.21}
-          x={-992.101}
-          y={-446.113}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={274.777 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-e"
-          width={1885.26}
-          height={1261.87}
-          x={-316.214}
-          y={79.613}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={43.65 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-f"
-          width={1985.69}
-          height={1415.51}
-          x={-367.503}
-          y={117.367}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={68.65 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-g"
-          width={2029.84}
-          height={903.678}
-          x={-470.394}
-          y={642.827}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={43.65 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-h"
-          width={1437.1}
-          height={1120.51}
-          x={339.06}
-          y={-618.092}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={54.917 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-j"
-          width={1230}
-          height={896.237}
-          x={669.574}
-          y={-667.229}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={19.221 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-l"
-          width={1158.3}
-          height={1093.91}
-          x={611.223}
-          y={-607.643}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={15 * blurScale} />
-        </filter>
-        <filter
-          id="bg1-n"
-          width={1102.3}
-          height={1039.38}
-          x={625.773}
-          y={-621.768}
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur result="effect1" stdDeviation={19.221 * blurScale} />
-        </filter>
         <radialGradient
           id="bg1-i"
           cx={0}
